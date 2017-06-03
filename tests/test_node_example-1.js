859bbefd5abcd3f593e9a5a2d638a9827c0fcee1
Christopher Diggins <christopher.diggins@autodesk.com>
2017-06-03 14:37:53 -0400

    Updating runkit example.

--- tests/test_node_example-1.js
+++ tests/test_node_example-1.js
@@ -1,7 +1,7 @@
 // Load the Myna module and all grammars
 var m = require('myna-parser');
 
-// Load the JSON grammar/parser
+// Load the JSON grammar
 require('myna-parser/grammars/grammar_json')(m);
 
 // Get the JSON parser 
@@ -10,13 +10,5 @@ var parser = m.parsers.json;
 // Define some input 
 var input = '{ "integer":42, "greeting":"hello", "truth":false, "array":[1,2,3] }';
 
-// Get a parse-tree 
-var ast = parser(input);
-
-// Print the parse tree 
-console.log("\nAbstract Syntax Tree\n")
-console.log(ast.toString());
-
-// Print the Parsing Expression Grammar 
-console.log("\nJSON Grammar in PEG Form\n")
-console.log(m.grammarToString('json'));
+// Output the generated AST 
+console.log(parser(input).toString());
